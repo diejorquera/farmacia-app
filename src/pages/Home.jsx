146ad810697a75d564fa09882f61ac0ea946c658
Diego Jorquera <diegojorquera19@gmail.com>
2025-12-13 23:24:18 -0300

    Fix FarmaciaResultado md:pt-10

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,6 @@ export default function Home() {
   return (
     <div className="min-h-screen antialiased font-montserrat">
       <FarmaciaBuscador />
-
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 max-w-screen-xl">
         <div className="py-6 sm:py-8 lg:py-10 space-y-6 sm:space-y-8">
           <RegionesGrid />
